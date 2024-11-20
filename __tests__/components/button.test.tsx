c3202b351759fb287f43f8664e2d7df9e91faab9
Paul van de Ven <paul@get-protocol.io>
2024-11-20 12:47:19 +0100

    button example tests

---
+++ __tests__/components/button.test.tsx
@@ -0,0 +1,39 @@
+import "@testing-library/jest-dom";
+import { render, RenderResult, screen } from "@testing-library/react";
+import Button from "../../components/Button";
+
+describe("Button", () => {
+  let tree: RenderResult;
+  let clicked: Boolean;
+
+  const text = "TESTButton";
+
+  beforeEach(() => {
+    clicked = false;
+    tree = render(
+      <Button
+        text={text}
+        onClick={() => {
+          clicked = true;
+        }}
+      />,
+    );
+  });
+
+  it("renders without changes", () => {
+    expect(tree.container).toMatchSnapshot();
+  });
+  it("renders a button with prop text", () => {
+    const button = screen.getByText(text);
+    expect(button).toBeInTheDocument();
+  });
+
+  it("call the onClick callback when clicking the button", () => {
+    const button = screen.getByText(text);
+    expect(button).toBeInTheDocument();
+
+    button.click();
+
+    expect(clicked).toBe(true);
+  });
+});
--- components/Button.tsx
+++ components/Button.tsx
@@ -1,9 +1,13 @@
-export default function Button(
-  props: React.PropsWithChildren<{ text: string; onClick: () => void }>,
-): JSX.Element {
+export default function Button({
+  text,
+  onClick = () => {},
+}: React.PropsWithChildren<{
+  text: string;
+  onClick?: () => void;
+}>): JSX.Element {
   return (
-    <button className='btn' onClick={props.onClick}>
-      {props.text}
+    <button className='btn' onClick={onClick}>
+      {text}
     </button>
   );
 }
